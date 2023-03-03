f26c3872af4b6fa439bb564a92a54e78f9e31356
Samudi-Hansani <samudi@creativesoftware.com>
2023-03-03 09:11:45 +0530

    initial commit

---
+++ src/main/java/com/skillup/profilemanagementservice/ProfileManagementServiceApplication.java
@@ -0,0 +1,13 @@
+package com.skillup.profilemanagementservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProfileManagementServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProfileManagementServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/skillup/profilemanagementservice/ProfileManagementServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skillup.profilemanagementservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProfileManagementServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
